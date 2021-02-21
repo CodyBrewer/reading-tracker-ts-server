5b43ef47bfad368a5dc434ee1b4f5f43a6da9330
Cody Brewer <codybrewerdev@gmail.com>
2021-02-21 16:42:34 -0700

    feat(entity/user.ts): add entity columns to user
    uuid, username, email, password, public.

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,16 +1,41 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
+import { Entity, PrimaryGeneratedColumn, Column, BeforeInsert, BaseEntity } from 'typeorm'
+import { v4 as uuid } from 'uuid'
+import bcrypt from 'bcrypt'
 
 @Entity('users')
-export class User {
+export class User extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number
 
-  @Column()
-  firstName: string
+  @Column({ type: 'uuid' })
+  uuid: string
+
+  @BeforeInsert()
+  async generateUUID(): Promise<void> {
+    this.uuid = await uuid()
+  }
 
   @Column()
-  lastName: string
+  username: string
 
   @Column()
-  age: number
+  email: string
+
+  @Column()
+  password: string
+
+  @BeforeInsert()
+  async hashPassword(): Promise<void> {
+    this.password = await bcrypt.hashSync(this.password, bcrypt.genSaltSync(10))
+  }
+
+  @Column()
+  public: boolean
+
+  toJSON() {
+    return {
+      ...this,
+      password: undefined
+    }
+  }
 }
