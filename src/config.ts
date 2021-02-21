70b3ebd3bde972741da84898745d2a453cc8086f
Cody Brewer <codybrewerdev@gmail.com>
2021-02-21 16:40:25 -0700

    feat(config.ts): add jwt secret to config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,9 @@
 import dotenv from 'dotenv'
+import { Secret } from 'jsonwebtoken'
 dotenv.config()
 
 export const NODE_ENV = String(process.env.NODE_ENV) || 'development'
-export const IS_DEV = NODE_ENV === 'development' ? true : false
+export const IS_DEV = NODE_ENV === 'development'
 export const PORT = Number(process.env.PORT) || 5000
 export const DB_URL = String(process.env.DATABASE_URL) || ''
+export const SECRET: Secret = String(process.env.JWT_SECRET) || ''
